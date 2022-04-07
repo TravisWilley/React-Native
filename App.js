2b884583630f00b3197fde1835f648f8f9807a6d
Travis Willey <Traviswilley24@yahoo.com>
2022-04-07 16:35:56 -0700

    trial

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -13,8 +14,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#0ff',
   },
 });
