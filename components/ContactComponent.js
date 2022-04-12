a0d44ed63a8286cf8ed118cbab57a57ae46c225b
Travis Willey <Traviswilley24@yahoo.com>
2022-04-11 19:35:09 -0700

    Setting Up Redux

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -3,11 +3,6 @@ import { ScrollView, Text } from 'react-native';
 import { Card } from 'react-native-elements';
 
 class Contact extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   static navigationOptions = {
     title: 'Contact Us',
   };
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://localhost:3001/';
+export const baseUrl = 'http://10.101.9.221:3001/';
